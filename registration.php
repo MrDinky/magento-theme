46e10814dc0e572695d6ce06b3599e99a11aa27c
Dmitry Fedorenko <dev.fedorenko@gmail.com>
2018-09-04 09:51:57 +0300

    Init

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+ 
+\Magento\Framework\Component\ComponentRegistrar::register(
+ 
+  \Magento\Framework\Component\ComponentRegistrar::THEME,
+ 
+  'frontend/Fedorenko/iwd-theme_luma-child',
+ 
+  __DIR__
+);
